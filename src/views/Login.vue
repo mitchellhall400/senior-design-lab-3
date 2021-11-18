44a57390da248362f5e110ab1afb0fc6cb53e4a1
Mitchell Hall <mitchell-a-hall@uiowa.edu>
2021-11-18 15:21:38 -0600

    Added forget password

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -19,98 +19,128 @@
             <div class="body-1">Sign Up</div>
           </v-tab>
           <v-tab-item>
-            <v-card class="px-4">
+            <v-card class="px-4" width="600px">
               <v-card-text>
                 <v-form ref="loginForm" v-model="valid" lazy-validation>
-                  <v-row>
-                    <v-col cols="12">
-                      <v-text-field
-                        v-model="loginEmail"
-                        :rules="loginEmailRules"
-                        label="E-mail"
-                        required
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                      <v-text-field
-                        v-model="loginPassword"
-                        :append-icon="show1 ? 'eye' : 'eye-off'"
-                        :rules="[rules.required, rules.min]"
-                        :type="show1 ? 'text' : 'password'"
-                        name="input-10-1"
-                        label="Password"
-                        hint="At least 8 characters"
-                        counter
-                        @click:append="show1 = !show1"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col class="d-flex" cols="12" sm="6" xsm="12"> </v-col>
+                  <v-text-field
+                    v-model="loginEmail"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="loginPassword"
+                    :append-icon="show1 ? 'eye' : 'eye-off'"
+                    :rules="[rules.required, rules.min]"
+                    :type="show1 ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="Password"
+                    hint="At least 8 characters"
+                    ref="email"
+                    counter
+                    @click:append="show1 = !show1"
+                  ></v-text-field>
+                  <v-card-actions class="pt-4">
                     <v-spacer></v-spacer>
-                    <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
-                      <v-btn
-                        block
-                        :disabled="!valid"
-                        color="secondary"
-                        @click="validateLogin"
-                      >
-                        Login
-                      </v-btn>
-                    </v-col>
-                  </v-row>
+                    <v-dialog v-model="dialog" persistent max-width="600px">
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn color="secondary" text v-bind="attrs" v-on="on">
+                          Forget Password
+                        </v-btn>
+                      </template>
+                      <v-card>
+                        <v-card-title>
+                          <span class="text-h5">Forget Password?</span>
+                        </v-card-title>
+                        <v-card-text>
+                          <v-form
+                            ref="forgetForm"
+                            v-model="valid"
+                            lazy-validation
+                          >
+                            Enter your email below and an email will be sent to
+                            you to reset your password.
+                            <v-text-field
+                              class="mt-4"
+                              v-model="forgetEmail"
+                              :rules="emailRules"
+                              label="E-mail"
+                              required
+                            ></v-text-field>
+                            <v-card-actions class="pt-4">
+                              <v-spacer></v-spacer>
+                              <v-btn
+                                color="secondary"
+                                text
+                                @click="dialog = false"
+                              >
+                                Close
+                              </v-btn>
+                              <v-btn
+                                :disabled="!valid"
+                                color="secondary"
+                                @click="validateForget"
+                              >
+                                Send Email
+                              </v-btn>
+                            </v-card-actions>
+                          </v-form>
+                        </v-card-text>
+                      </v-card>
+                    </v-dialog>
+                    <v-btn
+                      :disabled="!valid"
+                      color="secondary"
+                      @click="validateLogin"
+                    >
+                      Login
+                    </v-btn>
+                  </v-card-actions>
                 </v-form>
               </v-card-text>
             </v-card>
           </v-tab-item>
           <v-tab-item>
-            <v-card class="px-4">
+            <v-card class="px-4" width="600px">
               <v-card-text>
                 <v-form ref="signUpForm" v-model="valid" lazy-validation>
-                  <v-row>
-                    <v-col cols="12">
-                      <v-text-field
-                        v-model="email"
-                        :rules="emailRules"
-                        label="E-mail"
-                        required
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                      <v-text-field
-                        v-model="password"
-                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                        :rules="[rules.required, rules.min]"
-                        :type="show1 ? 'text' : 'password'"
-                        name="input-10-1"
-                        label="Password"
-                        hint="At least 8 characters"
-                        counter
-                        @click:append="show1 = !show1"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                      <v-text-field
-                        block
-                        v-model="verify"
-                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                        :rules="[rules.required, rules.min]"
-                        :type="show1 ? 'text' : 'password'"
-                        name="input-10-1"
-                        label="Confirm Password"
-                        counter
-                        @click:append="show1 = !show1"
-                      ></v-text-field>
-                    </v-col>
+                  <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="password"
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required, rules.min]"
+                    :type="show1 ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="Password"
+                    hint="At least 8 characters"
+                    counter
+                    @click:append="show1 = !show1"
+                  ></v-text-field>
+                  <v-text-field
+                    block
+                    v-model="verify"
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required, rules.min]"
+                    :type="show1 ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="Confirm Password"
+                    counter
+                    @click:append="show1 = !show1"
+                  ></v-text-field>
+                  <v-card-actions class="pt-4">
                     <v-spacer></v-spacer>
-                    <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
-                      <v-btn
-                        block
-                        :disabled="!valid"
-                        color="secondary"
-                        @click="validateSignUp"
-                        >Register</v-btn
-                      >
-                    </v-col>
-                  </v-row>
+                    <v-btn
+                      :disabled="!valid"
+                      color="secondary"
+                      @click="validateSignUp"
+                      >Register</v-btn
+                    >
+                  </v-card-actions>
                 </v-form>
               </v-card-text>
             </v-card>
@@ -125,12 +155,28 @@
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
-} from 'firebase/auth';
-import {auth} from '../firebase'
+  sendPasswordResetEmail,
+} from "firebase/auth";
+import { auth } from "../firebase";
 
 export default {
   name: "Login",
   methods: {
+    validateForget() {
+      if (this.$refs.forgetForm.validate()) {
+        sendPasswordResetEmail(auth, this.forgetEmail)
+          .then(() => {
+            this.$root.toast.show({ message: "Email sent!" })
+            this.dialog = false
+          })
+          .catch((error) => {
+            this.$root.toast.show({ message: error.message })
+          })
+      }
+      else {
+        this.$root.toast.show({ message: "Not sent!" })
+      }
+    },
     validateLogin() {
       if (this.$refs.loginForm.validate()) {
         signInWithEmailAndPassword(auth, this.loginEmail, this.loginPassword)
@@ -150,15 +196,14 @@ export default {
             .then(() => {
               this.$root.toast.show({
                 message: "Succesfully created an account!",
-              })
+              });
               this.$router.push("/dashboard");
             })
             .catch((error) => {
-              this.$root.toast.show({ message: error.message })
-            })
-        }
-        else {
-          this.$root.toast.show({ message: "Passwords do not match." })
+              this.$root.toast.show({ message: error.message });
+            });
+        } else {
+          this.$root.toast.show({ message: "Passwords do not match." });
         }
       }
     },
@@ -181,10 +226,8 @@ export default {
     verify: "",
     loginEmail: "",
     loginPassword: "",
-    loginEmailRules: [
-      (v) => !!v || "Required",
-      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
-    ],
+    forgetEmail: "",
+    dialog: false,
     emailRules: [
       (v) => !!v || "Required",
       (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
