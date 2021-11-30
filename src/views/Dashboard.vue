2484d928be8b5c9f5beab50812d7d778476adf97
Mitchell Hall <mitchell-a-hall@uiowa.edu>
2021-11-30 17:17:17 -0600

    Cleaned up dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -25,38 +25,34 @@
     </div>
     <template v-id="polls.length" v-for="(poll, index) in polls">
       <v-card class="ma-4" :key="index" elevation="8">
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="text-h4 mb-1">{{
-              poll.title
-            }}</v-list-item-title>
-            <v-list-item v-if="poll.description.replace(/\s/g, '').length"
-              ><v-icon class="pa-2">mdi-comment-text</v-icon>
-              <div class="clamp-text">{{ poll.description }}</div></v-list-item
-            >
-            <v-list-item v-if="poll.location.replace(/\s/g, '').length"
-              ><v-icon class="pa-2">mdi-map-marker</v-icon
-              >{{ poll.location }}</v-list-item
-            >
-            <v-list-item
-              ><v-icon class="pa-2">mdi-calendar-collapse-horizontal</v-icon
-              >Polling from {{ poll.window_date_start }} to
-              {{ poll.window_date_end }}</v-list-item
-            >
-            <v-list-item v-if="poll.close_date.replace(/\s/g, '').length"
-              ><v-icon class="pa-2">mdi-calendar-end</v-icon> Poll closes on
-              {{ poll.close_date }} at
-              {{ getTmz24Time(poll.close_time, poll.timezone) }}
-            </v-list-item>
-            <v-list-item v-if="poll.published"
-              ><v-icon class="pa-2">mdi-publish</v-icon>Published</v-list-item
-            >
-            <v-list-item v-else
-              ><v-icon class="pa-2">mdi-publish-off</v-icon>Not
-              published</v-list-item
-            >
-          </v-list-item-content>
+        <v-card-title class="primary">
+          {{ poll.title }}
+        </v-card-title>
+        <v-list-item v-if="poll.description.replace(/\s/g, '').length"
+          ><v-icon class="pa-2">mdi-comment-text</v-icon>
+          <div class="clamp-text">{{ poll.description }}</div></v-list-item
+        >
+        <v-list-item v-if="poll.location.replace(/\s/g, '').length"
+          ><v-icon class="pa-2">mdi-map-marker</v-icon
+          >{{ poll.location }}</v-list-item
+        >
+        <v-list-item
+          ><v-icon class="pa-2">mdi-calendar-collapse-horizontal</v-icon>Polling
+          from {{ poll.window_date_start }} to
+          {{ poll.window_date_end }}</v-list-item
+        >
+        <v-list-item v-if="poll.close_date.replace(/\s/g, '').length"
+          ><v-icon class="pa-2">mdi-calendar-end</v-icon> Poll closes on
+          {{ poll.close_date }} at
+          {{ getTmz24Time(poll.close_time, poll.timezone) }}
         </v-list-item>
+        <v-list-item v-if="poll.published"
+          ><v-icon class="pa-2">mdi-publish</v-icon>Published</v-list-item
+        >
+        <v-list-item v-else
+          ><v-icon class="pa-2">mdi-publish-off</v-icon>Not
+          published</v-list-item
+        >
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn icon color="secondary" @click="copyText(index)">
