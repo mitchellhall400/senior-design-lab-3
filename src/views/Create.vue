7bf3dcd30e557c224a266223937fdcbb05dc191c
matt-hall0100 <matt.hall0100@gmail.com>
2021-12-01 17:09:28 -0600

    Fixed Create page format to match them of other pages.

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,190 +1,195 @@
 <template>
   <v-container>
-    <v-card class="ma-4 pa-4" elevation="8">
-      <v-form>
-        <div class="text-h5 mb-4">Create New Poodle</div>
-        <v-text-field
-          v-model="title"
-          label="Title"
-          prepend-icon="mdi-label"
-          filled
-          :rules="[rules.required, rules.min]"
-        ></v-text-field>
-        <v-textarea
-          v-model="description"
-          label="Description"
-          prepend-icon="mdi-comment-text"
-          auto-grow
-          filled
-        ></v-textarea>
-        <v-text-field
-          v-model="location"
-          label="Location"
-          prepend-icon="mdi-map-marker"
-          filled
-        ></v-text-field>
-        <v-select
-          v-model="timezone"
-          label="Time Zone"
-          prepend-icon="mdi-map-clock"
-          :items="timezones"
-          filled
-        ></v-select>
-        <v-row>
-          <v-col>
-            <v-text-field
-              v-model="votesPerTimeslot"
-              label="Number of Votes Per Time Slot"
-              prepend-icon="mdi-check-all"
-              filled
-            ></v-text-field>
-            <v-select
-              v-model="selectedTimeOption"
-              label="Create time slots based on:"
-              prepend-icon="mdi-application-cog"
-              :items="timeslotOptions"
-              filled
-            ></v-select>
-          </v-col>
-          <v-col>
-            <v-text-field
-              v-model="votesPerUser"
-              label="Number of Votes Per User"
-              filled
-            ></v-text-field>
-            <v-text-field
-              v-model="timeslotCreationNumber"
-              filled
-              :label="selectedTimeOptionLabel()"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-combobox
-          v-model="emails"
-          label="Poodlers Email's"
-          prepend-icon="mdi-account-group"
-          hint="Enter individually or a comma separated list"
-          @change="verifyCombo()"
-          append-icon=""
-          chips
-          clearable
-          multiple
-          filled
-          auto-grow
-        >
-          <template v-slot:selection="{ item }">
-            <v-chip
-              close
-              outlined
-              @click:close="remove(item)"
-              @input="alert('sadaf')"
-            >
-              {{ item }}
-            </v-chip>
-          </template>
-        </v-combobox>
-        <v-list class="ml-0 pl-0 pr-0">
-          <h3>Poodle - Date Range</h3>
-          <v-list-item class="ml-0 pl-0 mr-0 pr-0">
-            <v-list-item-icon class="mr-0 pr-2">
-              <v-icon>mdi-calendar-collapse-horizontal</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
+    <v-card class="ma-4" elevation="8">
+      <v-card-title class="primary"> Create New Poodle </v-card-title>
+      <v-list-item-content class="ma-4">
+        <v-form>
+          <v-text-field
+            v-model="title"
+            label="Title"
+            prepend-icon="mdi-label"
+            filled
+            :rules="[rules.required, rules.min]"
+          ></v-text-field>
+          <v-textarea
+            v-model="description"
+            label="Description"
+            prepend-icon="mdi-comment-text"
+            auto-grow
+            filled
+          ></v-textarea>
+          <v-text-field
+            v-model="location"
+            label="Location"
+            prepend-icon="mdi-map-marker"
+            filled
+          ></v-text-field>
+          <v-select
+            v-model="timezone"
+            label="Time Zone"
+            prepend-icon="mdi-map-clock"
+            :items="timezones"
+            filled
+          ></v-select>
+          <v-row>
+            <v-col>
               <v-text-field
-                v-model="dateRangeText"
-                label="Poodle Date Range"
+                v-model="votesPerTimeslot"
+                label="Number of Votes Per Time Slot"
+                prepend-icon="mdi-check-all"
                 filled
-                hide-details
-                readonly
               ></v-text-field>
-              <v-date-picker
-                v-model="dates"
-                class="remove-top"
-                @input="updateDateRangeText()"
-                range
-                no-title
-                color="primary"
-                full-width
-              ></v-date-picker>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        <v-list class="ml-0 pl-0 pr-0">
-          <h3>Poodle - Time Range</h3>
-          <v-list-item class="ml-0 pl-0 mr-0 pr-0">
-            <v-list-item-icon class="mr-0 pr-2">
-              <v-icon>mdi-timeline-clock</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-row>
-                <v-col>
-                  <h4>Start:</h4>
-                  <v-time-picker
-                    v-model="timeRangeStart"
-                    color="primary"
-                    full-width
-                    ampm-in-title
-                    :max="timeRangeStop"
-                  ></v-time-picker>
-                </v-col>
-                <v-col>
-                  <h4>End:</h4>
-                  <v-time-picker
-                    v-model="timeRangeStop"
-                    color="primary"
-                    full-width
-                    ampm-in-title
-                    :min="timeRangeStart"
-                  ></v-time-picker>
-                </v-col>
-              </v-row>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        <v-list class="ml-0 pl-0 pr-0">
-          <h3>Poodle - Submission Close</h3>
-          <v-list-item class="ml-0 pl-0 mr-0 pr-0">
-            <v-list-item-icon class="mr-0 pr-2">
-              <v-icon>mdi-calendar-end</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-row>
-                <v-col>
-                  <h4>Date:</h4>
-                  <v-date-picker
-                    v-model="closeDate"
-                    class="stand-height"
-                    color="primary"
-                    full-width
-                  ></v-date-picker>
-                </v-col>
-                <v-col>
-                  <h4>Time:</h4>
-                  <v-time-picker
-                    v-model="closeTime"
-                    class="stand-height"
-                    color="primary"
-                    full-width
-                    ampm-in-title
-                  ></v-time-picker>
-                </v-col>
-              </v-row>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        <v-checkbox 
-          v-model="published"
-          name="publish" 
-          label="Publish with creation"
-        ></v-checkbox>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn @click="$router.push('/dashboard')" color="secondary" outlined
-            >Cancel
-          </v-btn>
-          <v-btn @click.prevent="createPoll" color="secondary">Create</v-btn>
-        </v-card-actions>
-      </v-form>
+              <v-select
+                v-model="selectedTimeOption"
+                label="Create time slots based on:"
+                prepend-icon="mdi-application-cog"
+                :items="timeslotOptions"
+                filled
+              ></v-select>
+            </v-col>
+            <v-col>
+              <v-text-field
+                v-model="votesPerUser"
+                label="Number of Votes Per User"
+                filled
+              ></v-text-field>
+              <v-text-field
+                v-model="timeslotCreationNumber"
+                filled
+                :label="selectedTimeOptionLabel()"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-combobox
+            v-model="emails"
+            label="Poodlers Email's"
+            prepend-icon="mdi-account-group"
+            hint="Enter individually or a comma separated list"
+            @change="verifyCombo()"
+            append-icon=""
+            chips
+            clearable
+            multiple
+            filled
+            auto-grow
+          >
+            <template v-slot:selection="{ item }">
+              <v-chip
+                close
+                outlined
+                @click:close="remove(item)"
+                @input="alert('sadaf')"
+              >
+                {{ item }}
+              </v-chip>
+            </template>
+          </v-combobox>
+          <v-list class="ml-0 pl-0 pr-0">
+            <h3>Poodle - Date Range</h3>
+            <v-list-item class="ml-0 pl-0 mr-0 pr-0">
+              <v-list-item-icon class="mr-0 pr-2">
+                <v-icon>mdi-calendar-collapse-horizontal</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-text-field
+                  v-model="dateRangeText"
+                  label="Poodle Date Range"
+                  filled
+                  hide-details
+                  disabled
+                ></v-text-field>
+                <v-date-picker
+                  v-model="dates"
+                  class="remove-top"
+                  @input="updateDateRangeText()"
+                  range
+                  no-title
+                  color="primary"
+                  full-width
+                ></v-date-picker>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <v-list class="ml-0 pl-0 pr-0">
+            <h3>Poodle - Time Range</h3>
+            <v-list-item class="ml-0 pl-0 mr-0 pr-0">
+              <v-list-item-icon class="mr-0 pr-2">
+                <v-icon>mdi-timeline-clock</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-row>
+                  <v-col>
+                    <h4>Start:</h4>
+                    <v-time-picker
+                      v-model="timeRangeStart"
+                      color="primary"
+                      full-width
+                      ampm-in-title
+                      :max="timeRangeStop"
+                    ></v-time-picker>
+                  </v-col>
+                  <v-col>
+                    <h4>End:</h4>
+                    <v-time-picker
+                      v-model="timeRangeStop"
+                      color="primary"
+                      full-width
+                      ampm-in-title
+                      :min="timeRangeStart"
+                    ></v-time-picker>
+                  </v-col>
+                </v-row>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <v-list class="ml-0 pl-0 pr-0">
+            <h3>Poodle - Submission Close</h3>
+            <v-list-item class="ml-0 pl-0 mr-0 pr-0">
+              <v-list-item-icon class="mr-0 pr-2">
+                <v-icon>mdi-calendar-end</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-row>
+                  <v-col>
+                    <h4>Date:</h4>
+                    <v-date-picker
+                      v-model="closeDate"
+                      class="stand-height"
+                      color="primary"
+                      full-width
+                    ></v-date-picker>
+                  </v-col>
+                  <v-col>
+                    <h4>Time:</h4>
+                    <v-time-picker
+                      v-model="closeTime"
+                      class="stand-height"
+                      color="primary"
+                      full-width
+                      ampm-in-title
+                    ></v-time-picker>
+                  </v-col>
+                </v-row>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <v-checkbox
+            v-model="published"
+            name="publish"
+            label="Publish with creation"
+          ></v-checkbox>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              @click="$router.push('/dashboard')"
+              color="secondary"
+              outlined
+              >Cancel
+            </v-btn>
+            <v-btn @click.prevent="createPoll" color="secondary">Create</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-list-item-content>
     </v-card>
   </v-container>
 </template>
@@ -338,17 +343,17 @@ export default {
       var timeSlotPerDay = "";
       if (this.selectedTimeOption == "desired number of time slots per day") {
         timeSlotPerDay = this.timeslotCreationNumber;
-      }
-      else {
+      } else {
         timeSlotDuration = this.timeslotCreationNumber;
       }
 
       push(ref(db, "polls"), {
-        close_date: dCur.getFullYear() +
+        close_date:
+          dCur.getFullYear() +
           "-" +
           (dCur.getMonth() + 1) +
           "-" +
-          (dCur.getDate()+1),
+          (dCur.getDate() + 1),
         close_time: this.closeTime,
         created_by: auth.currentUser.email,
         description: this.description,
