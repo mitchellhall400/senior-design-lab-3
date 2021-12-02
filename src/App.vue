6ea73f443359c40f5580c036a2a89cf0187b0199
Mitchell Hall <mitchell-a-hall@uiowa.edu>
2021-12-02 14:20:57 -0600

    Added toolbar-title routing

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,67 @@
 <template>
-  <v-app id='inspire'>
-    <v-app-bar app class='primary'>
-       <v-img
+  <v-app id="inspire">
+    <v-app-bar app class="primary">
+      <v-img
+        style="cursor: pointer"
+        @click="$router.push('/dashboard')"
         class="mx-2"
         src="./assets/french-poodle.png"
         max-height="40"
         max-width="40"
         contain
       ></v-img>
-      <v-toolbar-title>Poodle Poll</v-toolbar-title>
+      <v-toolbar-title
+        style="cursor: pointer"
+        @click="$router.push('/dashboard')"
+        ><h2>Poodle Poll</h2></v-toolbar-title
+      >
       <v-spacer></v-spacer>
-      <v-btn
-        v-if='isSignedIn'
-        class='secondary'
-        @click="logout"
-      >Logout</v-btn>
-      <v-btn
-        icon
-        @click="toggleTheme"
-      ><v-icon>mdi-brightness-4</v-icon></v-btn>
+      <v-btn v-if="isSignedIn" class="secondary" @click="logout">Logout</v-btn>
+      <v-btn icon @click="toggleTheme"><v-icon>mdi-brightness-4</v-icon></v-btn>
     </v-app-bar>
-    <v-main class='ma-4'><router-view /></v-main>
-    <toast ref="toast"/>
+    <v-main class="ma-4"><router-view /></v-main>
+    <toast ref="toast" />
   </v-app>
 </template>
 
 <script>
-import toast from './components/Toast.vue'
-import { signOut, getAuth } from 'firebase/auth'
+import toast from "./components/Toast.vue";
+import { signOut, getAuth } from "firebase/auth";
 export default {
-  components:{
-    toast
+  components: {
+    toast,
   },
   mounted() {
-    this.$root.toast = this.$refs.toast
+    this.$root.toast = this.$refs.toast;
   },
   computed: {
     isSignedIn() {
-      return this.$route.name !== 'Login' && this.$route.name !== 'Poll'
+      return this.$route.name !== "Login" && this.$route.name !== "Poll";
     },
   },
   methods: {
     toggleTheme() {
-      this.$vuetify.theme.dark = !(this.$vuetify.theme.dark)
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
     },
     logout() {
       const auth = getAuth();
-      signOut(auth).then(() => {
-        this.$router.push('/')
-      })
-      .catch(error => {
-        alert(error.message)
-        this.$router.push('/')
-      })
+      signOut(auth)
+        .then(() => {
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          alert(error.message);
+          this.$router.push("/");
+        });
     },
   },
   metaInfo: {
-    tile: 'Login',
-    titleTemplate: '%s | Poodle Poll',
+    tile: "Login",
+    titleTemplate: "%s | Poodle Poll",
     htmlAttrs: {
-      lang: 'en-US'
+      lang: "en-US",
     },
-    meta: [
-      { name: 'theme-color', content: '#446664' },
-    ]
+    meta: [{ name: "theme-color", content: "#446664" }],
   },
-}
+};
 </script>
